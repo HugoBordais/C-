8969021e72eedc19cb7213964472ebf9937301ee
Hugo <hugo.bordais@ynov.com>
2018-11-14 16:07:34 +0100

    Init git

---
+++ TP3/AfficheContenu.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections;
+
+namespace TP3
+{
+    static class AfficheContenu
+    {
+        public static void Contenu(this ArrayList list)
+        {
+            string result = "Salut";
+            
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ TP3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Concurrent;
+
+namespace TP3
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            ArrayList l = new ArrayList();
+            for (int i = 0; i < 101; i++)
+            {
+                l.Add(i);
+            }
+            l.Contenu();
+        }
+    }
+}
